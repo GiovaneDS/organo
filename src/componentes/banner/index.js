cbeb615e3c9a58de0a516ec4f80ed74ece5b3040
GiovaneDS <giovannepersia@hotmail.com>
2024-09-04 01:38:31 -0300

    criação do projeto e primeiros componentes

---
+++ src/componentes/banner/index.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+const Banner = () => { 
+    //JSX
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="BANNER PRINCIPAL"/>
+        </header>
+        
+    )
+}
+
+export default Banner
+
